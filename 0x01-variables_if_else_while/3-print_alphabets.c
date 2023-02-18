323c60dcbe85fd2603a744685e712cc1181a9e26
dounia <bendib.dounia@gmail.com>
2023-02-17 17:12:09 -0800

    Task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; cl <= 'z'; cl++)
+		putchar(cl);
+
+	for (cl = 'A'; cl <= 'Z'; cl++)
+		putchar(cl);
+	
+	putchar('\n');
+
+	return (0);
+}

3e2f065a75756f6582588dea363a32d79abe2f46
dounia <bendib.dounia@gmail.com>
2023-02-17 18:32:40 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <sdtio.h>
+
+/**
+ * main - prints single digits in base 10.
+ *
+ * Return: Alawys 0
+ */
+int main(void)
+{
+	int N;
+	
+	for (N = 0; N < 10; N++)
+		putchar((N % 10) + '0');
+	putchar('\n');
+	return (0);
+}

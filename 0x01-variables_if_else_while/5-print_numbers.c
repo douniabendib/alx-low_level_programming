4e965bc6b521cadc4cf88306fa208cb46b9af803
dounia <bendib.dounia@gmail.com>
2023-02-17 17:39:41 -0800

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Digit number.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i);
+	
+	return (0);
+}

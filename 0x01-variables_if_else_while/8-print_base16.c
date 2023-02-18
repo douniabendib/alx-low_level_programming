696ed5cc7357acc126dff0c8d054a3caa63adabe
dounia <bendib.dounia@gmail.com>
2023-02-17 18:59:54 -0800

    base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int M;
+	char le;
+
+	for (M = 0;M < 10 ;M++)
+		putchar ((M % 10) + '0');
+	for (le = 'a'; le <= 'f';le++)
+		putchar(le);
+	putchar('\n');
+	return (0);
+}

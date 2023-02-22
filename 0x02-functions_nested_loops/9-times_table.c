ca968e4aa75f3213278268c279cef9473ab76f0b
dounia <bendib.dounia@gmail.com>
2023-02-22 14:56:35 -0800

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,6 +6,8 @@
 void times_table(void)
 {
 int i, j, k;
+for (i = 0; i < 10; i++)
+{
 for (j = 0; j < 10; j++)
 {
 	k = j * i;
@@ -29,4 +31,5 @@ for (j = 0; j < 10; j++)
 	}
 }
 _putchar('\n');
+}
 }

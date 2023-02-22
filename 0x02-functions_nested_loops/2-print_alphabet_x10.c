16803157a15f73c410b2ad0228b0c48d9cb9fe63
dounia <bendib.dounia@gmail.com>
2023-02-22 07:15:02 -0800

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * void - ptints all alphabets 10 time.
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	}
+	_putchar('\n');
+}

9dc2cc3bd2e97b5dddfe1399a056ab4fb739be85
dounia <bendib.dounia@gmail.com>
2023-02-22 16:23:52 -0800

    times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ int i, j, k;
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
-}
+{
 for (j = 0; j <= n; j++)
 {
 k = j * i;
@@ -43,4 +43,6 @@ _putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
+}
+}
 }

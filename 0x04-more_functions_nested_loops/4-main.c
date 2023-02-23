f201d3e96c9321368423510d97dc7d1e865d6cac
dounia <bendib.dounia@gmail.com>
2023-02-23 13:52:05 -0800

    Print most numbers

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - print most numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_most_numbers();
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - print most num.
+ * return: void
+ */
+void print_most_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9' ; c++)
+{
+if (c != '4' && c != '2')
+_putchar(c);
+}
+_putchar('\n');
+}

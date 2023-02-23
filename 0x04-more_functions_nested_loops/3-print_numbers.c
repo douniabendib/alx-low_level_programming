0fe8299510b0c4017798da45959fa238178b160b
dounia <bendib.dounia@gmail.com>
2023-02-23 13:41:17 -0800

    Print Numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * print - print the numbers
+ * print_numbers - print the numbers
  * return: void
  */
 void print_numbers(void)
 {
 char i;
 for (i = '0'; i <= '9'; i++)
+{
 _putchar(i);
+}
 _putchar('\n');
 }

7321c7122545e0841d769b61368c83aaac5feca8
dounia <bendib.dounia@gmail.com>
2023-02-23 12:51:41 -0800

    Multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - checked the multiple.
+ * @a: first int
+ * @b: second int
+ * Return: multiple
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}

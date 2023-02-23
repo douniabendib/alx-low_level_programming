4b1ab33cf05ed83f771769fd957b89ea75d0d2a5
dounia <bendib.dounia@gmail.com>
2023-02-23 11:26:56 -0800

    Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check if alphabet is upper.
+ * @c:the character
+ * Return: 1 if c is upper,0 otherwise.
+ */
+
+int _isupper(int c)
+{
+return (c >= 'a' && c <= 'z');
+}

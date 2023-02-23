216b06055f7126d7e22cbaafab9c44d113ed0494
dounia <bendib.dounia@gmail.com>
2023-02-23 12:59:31 -0800

    Print the numbers

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - prints numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_numbers();
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print - print the numbers
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+_putchar(i);
+_putchar('\n');
+}

d312180d6fe59d19278716d31026aa4d146c55b4
dounia <bendib.dounia@gmail.com>
2023-03-10 17:34:37 -0800

    library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
-void _puts(char *str)
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void)
+void _puts(char *str)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
 }

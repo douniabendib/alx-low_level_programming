15ac11995b56aff73a0f67627bcd56d2d5bdd425
dounia <bendib.dounia@gmail.com>
2023-03-20 12:00:23 -0700

    Print Dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,18 +3,14 @@
 /**
  * print_dog - print a variable of type struct dog
  * @d: pointer to struct
- * @name: name to print
- * @age: age to print
- * @owner: owner to print
  */
 void print_dog(struct dog *d)
 {
 if (d == NULL)
-	return;
-        if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->owner == NULL)
-		d->owner = "(nil)";
-        d->owner = owner;
-	printf("Name: %s\nAge: %f\nOwner: %s\n",d->name,d->age,d->owner);
+return;
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }

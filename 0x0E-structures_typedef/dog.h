beb943db313293d606edae2649d4378387a72c2d
dounia <bendib.dounia@gmail.com>
2023-03-20 08:36:41 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifdef dog_h
+#define dog_h
+/**
+ * struct dog - dog info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: Longer description
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
-}
+};
+/**
+ * my_dog - typedef for struct dog
+ */
+typedef struct dog my_dog;
+#endif

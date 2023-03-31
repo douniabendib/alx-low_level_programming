867e7953a4f842672ddc19dcd73a36f60add510e
douniabendib <bendib.dounia@gmail.com>
2023-03-30 18:36:18 -0700

    list length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,11 +8,11 @@
  */
 size_t list_len(const list_t *h)
 {
- size_t elem = 0;
- while (h)
- {
- elem++;
- h = h->next;
- }
- return (elem);
+size_t elem = 0;
+while (h)
+{
+elem++;
+h = h->next;
+}
+return (elem);
 }

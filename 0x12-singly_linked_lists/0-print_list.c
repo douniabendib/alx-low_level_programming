480af1d144c6d33ad6d1fc404e9f50ae69f4f94e
douniabendib <bendib.dounia@gmail.com>
2023-03-30 18:28:48 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,15 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t node = 0;
-
-	while (h)
-	{
-	if (h->str == NULL)
-		printf("[0](nil)\n");
-	else
-		printf("[%u] %s \n", h->len, h->str);
-	h = h->next;
-	node++;
-	}
-	return (node);
+size_t node = 0;
+while (h)
+{
+if (h->str == NULL)
+printf("[0](nil)\n");
+else
+printf("[%u] %s \n", h->len, h->str);
+h = h->next;
+node++;
+}
+return (node);
 }

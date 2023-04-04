1398c3b39ce583fa966e4fbb435968d026c24d1e
douniabendib <bendib.dounia@gmail.com>
2023-04-03 19:13:29 -0700

    len list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,15 +1,13 @@
 #include "lists.h"
 /*
- * listint_len - returns the number of elements in a linked
- * @h: pointer of list 
+ * listint_len - length of linked list
+ * @h: pointer of list
  * Return: returns the number of list
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t node = 0;
 
-	if (h == NULL)
-		printf("The linked list is empty");
 	while (h != NULL)
 	{
 		node++;

dadb4f66184ca37f4610f90b7f6799bcd2004574
douniabendib <bendib.dounia@gmail.com>
2023-04-03 18:34:29 -0700

    print linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint- print linked list
+ * @h: pointer of lisl
+ * Return:the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	if (h == NULL)
+		printf("The linked list is empty");
+	while (h != NULL)
+	{
+		printf("%d \n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return(nodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+0x13-more isingly linked lists

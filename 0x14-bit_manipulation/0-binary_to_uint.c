cf0bde5b075ab2782809d2027bbfc4e870f2bd6b
douniabendib <bendib.dounia@gmail.com>
2023-04-09 09:17:08 -0700

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert binary to decimal number
+ * @b:pointer to a string of 0 and 1 chars
+ * Return: to converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	unsigned int decimal = 0;
+	if (!b) 
+		return (0);
+	for (a = 0 ; b[a] ; a++)
+	{
+		if (b[a] < '0' || b[a] > '1')
+			return (0);
+		decimal = 2 * decimal + (b[a] - '0');
+	}
+	return (decimal);
+}

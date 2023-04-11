0b30c8b72f2df3cf086238ec533b97e382e2b19c
douniabendib <bendib.dounia@gmail.com>
2023-04-10 19:58:07 -0700

    READ text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,22 +4,20 @@
  * read_textfile - reads a text file and prints
  * @filename : text file
  * @letters: number of letters
- * Return:0 when function fails to open or file is NULL 
+ * Return:0 when function fails to open or file is NULL
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *c;
-	ssize_t fp;
-	ssize_t li;
-	ssize_t ecr;
+	ssize_t fp, ecr, li;
 
 	fp = open(filename, O_RDONLY);
 	if (fp == -1)
-		return (1);
-	c = malloc(sizeof(char)* letters);
+		return (0);
+	c = malloc(sizeof(char) * letters);
 	li = read(fp, c, letters);
 	ecr = write(STDOUT_FILENO, c, li);
-	free (c);
-	close (fp);
+	free(c);
+	close(fp);
 	return (ecr);
 }

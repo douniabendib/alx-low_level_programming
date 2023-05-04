a9512a5eba28a025eb09da76f3d374d23432f4a3
douniabendib <bendib.dounia@gmail.com>
2023-05-04 14:15:57 -0700

    get endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - function that checks the endianness
+ * Return: 0 for big, 1 for little.
+ */
+int get_endianness(void)
+{
+	unsigned int a = 1;
+	char *d = (char *) & a;
+
+	return (*d);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);

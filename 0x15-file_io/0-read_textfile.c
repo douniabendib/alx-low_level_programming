b4a5c3192f24353516803ba635ccd241cbafd3fb
douniabendib <bendib.dounia@gmail.com>
2023-05-08 14:52:01 -0700

    How to read a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * read_textfile - function for read text from file
+ * @filename : file name to be read
+ * @letters :number of char want to read
+ * Return : 0 if failed or number of char
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t n;
+	char *buffer = malloc(sizeof(char) * letters);
+	FILE *fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		return (0);
+	}
+	n = read(fileno(fp), buffer, letters);
+	printf("%s", buffer);
+	fclose(fp);
+	free(buffer);
+	return (n);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,3 @@
+FILE I/O 
+how create file open read write close a file
+different Task in this file.

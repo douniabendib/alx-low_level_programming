75fd62e7f7db51e213ac5b3bcde1fc89d0e7fe42
douniabendib <bendib.dounia@gmail.com>
2023-05-08 17:21:17 -0700

    how to create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		len = strlen(text_content);
-		n = write(text_content, len, fp);
+		n = write(fp, text_content, len);
 	}
 	if (fp == -1 || n == -1)
 		return (-1);

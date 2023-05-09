d90ef5b9221548626e47a195123ac4ec4fae9acd
douniabendib <bendib.dounia@gmail.com>
2023-05-08 17:14:16 -0700

    how to create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,17 +13,15 @@ int create_file(const char *filename, char *text_content)
 
 	len = 0;
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fp == NULL)
+	if (fp == -1)
 	{
-		printf("Failed to create file %s.\n", filename);
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
 		len = strlen(text_content);
-		n = write(text_content, sizeof(char), len, fp);
+		n = write(text_content, len, fp);
 	}
-	printf("File %s created successfully.\n", filename);
 	if (fp == -1 || n == -1)
 		return (-1);
 	close(fp);

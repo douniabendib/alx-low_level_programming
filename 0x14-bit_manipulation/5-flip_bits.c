46744674c267185f30805f10bfe74b3f58810dce
bendib dounia <bendib.dounia@gmail.com>
2023-07-06 17:54:09 -0700

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits -function that need to flip to get from one number to another
+ * @n: number 1
+ * @m: number 2
+ * Return: number to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int a, b = 0;
+	unsigned long int cur;
+	unsigned long int e = n ^ m;
+
+	for (a = 63; a >= 0; a--)
+	{
+		cur = e >> a;
+		if (cur & 1)
+			b++;
+	}
+	return (b);
+}
